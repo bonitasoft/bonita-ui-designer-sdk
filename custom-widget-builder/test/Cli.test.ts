6852a85fc7b38775d900fdace3c77d9c5f311078
Jerome Cambon <jerome.p.cambon@gmail.com>
2021-05-14 13:57:10 +0200

    test CLI

---
+++ custom-widget-builder/test/Cli.test.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2021 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as os from "os";
+import {ExecException} from "child_process";
+const fs = require('fs');
+const { exec } = require('child_process');
+
+describe('CLI test', () => {
+
+  let tempDir: string;
+
+  beforeAll(() => {
+    tempDir = fs.mkdtempSync(os.tmpdir());
+  });
+
+  afterAll(() => {
+    fs.rmdirSync(tempDir, {recursive: true});
+  });
+
+  test('should handle correct parameters when generating a json properties file', async (done) => {
+    let command =
+      "build/src/starter.js gen-widget " +
+      "--propertiesFile build/test/pbInput.json " +
+      "--webComponentBundle test/resources/pb-input/lib/pb-input.es5.min.js " +
+      "--outputDir build/widget"
+    exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
+      expect(stdout.includes("Widget has been generated in build/widget")).toBeTruthy();
+      done();
+    });
+  });
+
+})
